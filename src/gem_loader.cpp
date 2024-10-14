2ce429851f25beda61d192dec0981d768f7936cc
xSparky911x <donsopher@hotmail.com>
2024-10-13 18:33:03 -0600

    add a load file

---
+++ src/gem_loader.cpp
@@ -0,0 +1,7 @@
+// Declare the function to register the module scripts
+void AddGatheringExperienceModuleScripts();
+
+void Addmod_gathering_experienceScripts()
+{
+    AddGatheringExperienceModuleScripts();
+}
